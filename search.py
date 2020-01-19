c36559b3ebaa68a8bd97df28f21daf1021093048
isso0424 <isso04240129@gmail.com>
2020-01-19 13:04:54 +0900

    v1

--- search.py
+++ search.py
@@ -1,6 +1,6 @@
 import requests
-import asyncio
 import lxml.html
+import shutil
 
 class ReaderMain:
     """
@@ -15,6 +15,7 @@ class ReaderMain:
         self.search_word = ""
         self.searcher = QiitaSearcher()
         self.search_results = []
+        self.article_number = 0
 
     def input_key_word(self):
         self.have_to_change_view_flag = False
@@ -29,15 +30,37 @@ class ReaderMain:
                     self.search_word = key_word
                     self.view_name = "search-results"
                 return
+    def get_divider(self, n):
+        s = ""
+        for i in range(0, n):
+            s += "="
+        return s
 
     def root_scene(self):
+        terminal_size = shutil.get_terminal_size()
         while self.continue_flag:
             if self.view_name == "before-search":
                 self.input_key_word()
-                if self.continue_flag:
-                    self.searcher.search_request(self.search_word)
             elif self.view_name == "search-results":
+                self.search_results = self.searcher.search_request(self.search_word)
                 self.output_search_results()
+                self.select_article()
+                print(self.get_divider(terminal_size[0]))
+                print(self.searcher.load_article(self.search_results[self.article_number]["url"]))
+                print(self.get_divider(terminal_size[0]))
+                self.view_name = "before-search"
+
+    def select_article(self):
+        while True:
+            n = input("記事の番号を入力してください\n>>>")
+            try:
+                if int(n) in range(1, len(self.search_results) + 2):
+                    self.article_number =  int(n)
+                    return
+                else:
+                    print("記事の番号にはありません")
+            except ValueError:
+                print("数値を入力してください")
 
 
     def output_search_results(self):
@@ -46,7 +69,9 @@ class ReaderMain:
         i = 1
         for detail in self.search_results:
             print("No.{}".format(i))
-            print()
+            print("Title:{}".format(detail["title"]))
+            print("Writer:{}".format(detail["writer"]))
+            i += 1
 
 
 
@@ -60,9 +85,36 @@ class QiitaSearcher:
             検索する単語
         """
         url = "https://qiita.com/search"
-        raw_html = requests.get(url)
+        params = {"q": word}
+        raw_html = requests.get(url, params)
         html = lxml.html.fromstring(raw_html.text)
-        #TODO: データパースしろカス
-        data = None
-        return data
+        results = []
 
+        for d in range(2, 12):
+            data_cache = {}
+
+            # 記事の各要素をdata_cacheへ保存
+            writer_name = html.xpath("/html/body/div[3]/div/div/div[1]/div[{}]/div[1]/a".format(d))[0].attrib["href"].strip("/")
+            data_cache["writer"] = writer_name
+
+            url = html.xpath("/html/body/div[3]/div/div/div[1]/div[{}]/div[2]/h1/a".format(d))[0].attrib["href"]
+            data_cache["url"] = url
+
+            title = html.xpath("/html/body/div[3]/div/div/div[1]/div[{}]/div[2]/h1".format(d))[0].text_content()
+            data_cache["title"] = title
+
+            # resultsへデータを保存
+            results.append(data_cache)
+
+        return results
+
+    def load_article(self, raw_url):
+        url = "https://qiita.com" + raw_url
+        access = requests.get(url)
+        html = lxml.html.fromstring(access.text)
+        txt = html.xpath("//section[@class='it-MdContent']")[0].text_content()
+        return txt
+
+if __name__ == '__main__':
+    reader = ReaderMain()
+    reader.root_scene()
